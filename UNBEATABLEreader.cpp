48e077d594e0b7aa0f62a5340a1f041bda4ab861
MelloBoo44 <melloboo33@gmail.com>
2026-03-24 14:18:50 -0500

    Halfway done with reading
    It can read in untill accuracies, and nothing more

--- UNBEATABLEreader.cpp
+++ UNBEATABLEreader.cpp
@@ -9,6 +9,8 @@
 #include <filesystem>
 #include <chrono>
 
+const int kMaxNumSongs = 200;
+
     //function defs
     std::string getFilepath();
     void ReadAndPrintSheet(std::ifstream &ArcadeScoreFile, std::string scoresFilePath);
@@ -56,7 +58,6 @@ std::string getFilepath() {
     return path;
 }
 
-//uhh on second thought dont actually do what you're doing ok thanks
 void ReadAndPrintSheet(std::ifstream &ArcadeScoreFile, std::string scoresFilePath) {
     ArcadeScoreFile.open(scoresFilePath);
     if(!ArcadeScoreFile) {
@@ -64,25 +65,103 @@ void ReadAndPrintSheet(std::ifstream &ArcadeScoreFile, std::string scoresFilePat
         std::exit(1);
     }
 
-    // char charRead{};
-    // std::println();
-    // while(ArcadeScoreFile && !(charRead==':')) {
-    //     ArcadeScoreFile.get(charRead);
-    //     std::print("{}",charRead);
-    // }
-    // ArcadeScoreFile.get(charRead);
-    // while(ArcadeScoreFile && !(charRead=='g')) {
-    //     ArcadeScoreFile.get(charRead);
-    //     std::print("{}",charRead);
-    // }
-    // ArcadeScoreFile.get(charRead);
-    // ArcadeScoreFile.get(charRead);
-    //
-    // std::println();
+    char readChar{};
+    int indent{};
+    int songIndex{};
+
+    std::string songName[kMaxNumSongs]{};
+    std::string difficultyName[kMaxNumSongs]{};
+    std::string modName[kMaxNumSongs]{};
+    std::string score[kMaxNumSongs]{};
+    double accuracy[kMaxNumSongs]{};
+    int maxCombo[kMaxNumSongs]{};
+    int Crit[kMaxNumSongs]{};
+    int Perf[kMaxNumSongs]{};
+    int Great[kMaxNumSongs]{};
+    int Good[kMaxNumSongs]{};
+    int Ok[kMaxNumSongs]{};
+    int Barely[kMaxNumSongs]{};
+    int Miss[kMaxNumSongs]{};
+    int Level[kMaxNumSongs]{};
 
 
 
+    ArcadeScoreFile >> readChar;
+    while(ArcadeScoreFile) {
+        if(readChar=='{') {
+            indent+=4;
+            std::println();
+            for (int i=0; i<indent; i++) {
+                std::print(" ");
+            }
+        }
+        if(readChar=='}') {
+            indent-=4;
+            std::println();
+            for (int i=0; i<indent; i++) {
+                std::print(" ");
+            }
+        }
+        if (indent==8 && readChar=='{') { //once we're a certain depth in the json, we know we're starting with the '{"song":"' bit, discard up to the actual name
+            for (int i=1; i<10; i++) { //magic number because :) lol
+                ArcadeScoreFile >> readChar;
+            }
+            //right now, we're ready to read the name!
 
+            while(ArcadeScoreFile && !(readChar=='/')) { //SONG NAME grabber
+                songName[songIndex]=songName[songIndex]+readChar;
+                ArcadeScoreFile >> readChar;
+            }
+            ArcadeScoreFile >> readChar; // discard '/'
+            while(ArcadeScoreFile && !(readChar=='\\')) { //DIFFICULTY grabber
+                difficultyName[songIndex]=difficultyName[songIndex]+readChar;
+                ArcadeScoreFile >> readChar;
+            }
+            ArcadeScoreFile >> readChar; // discard '\'
+            ArcadeScoreFile >> readChar; // discard '\' part 2
+            while(ArcadeScoreFile && !(readChar=='\"')) { //MODIFIER grabber
+                modName[songIndex]=modName[songIndex]+readChar;
+                ArcadeScoreFile >> readChar;
+            }
+            ArcadeScoreFile >> readChar; // discard until ':'
+            while(ArcadeScoreFile && !(readChar==':')) {
+                ArcadeScoreFile >> readChar;
+            }
+            ArcadeScoreFile >> readChar; //actaully discard the :
+            while(ArcadeScoreFile && !(readChar==',')) { //SCORE grabber
+                score[songIndex]=score[songIndex]+readChar;
+                ArcadeScoreFile >> readChar;
+            }
+            while(ArcadeScoreFile && !(readChar==':')) { //discard until :
+                ArcadeScoreFile >> readChar;
+            }
+            ArcadeScoreFile >> accuracy[songIndex]; //ACCURACY grabber
+
+            // ArcadeScoreFile >> maxCombo[songIndex];
+            // ArcadeScoreFile >> Crit[songIndex];
+            // ArcadeScoreFile >> Perf[songIndex];
+            // ArcadeScoreFile >> Great[songIndex];
+            // ArcadeScoreFile >> Good[songIndex];
+            // ArcadeScoreFile >> Ok[songIndex];
+            // ArcadeScoreFile >> Barely[songIndex];
+            // ArcadeScoreFile >> Miss[songIndex];
+            // ArcadeScoreFile >> Level[songIndex];
+
+            songIndex+=1;
+
+
+
+        }
+        if (indent>=8) {
+            std::print("{}", readChar);
+        }
+        ArcadeScoreFile >> readChar;
+    }
+
+
+    for (int i=0; i<songIndex; i++) {
+            std::println("{:21} | {:10} | {:10} | {:7} | {:.4f} | {:4} | {:4} | {:4} | {:4} | {:4} | {:4} | {:4} | {:4} | {:4}", songName[i], difficultyName[i], modName[i], score[i], accuracy[i], maxCombo[i], Crit[i], Perf[i], Great[i], Good[i], Ok[i], Barely[i], Miss[i], Level[i]);
+    }
 
 
 
